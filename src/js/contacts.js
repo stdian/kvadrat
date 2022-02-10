ae6ff80518af884f6f5111c6cb2ad571294eb786
Dmitriy Stepanov <stdian@mail.ru>
2022-02-10 09:58:02 +0800

    make contacts page

--- src/js/contacts.js
+++ src/js/contacts.js
@@ -1,2 +1,12 @@
+/* eslint-disable no-use-before-define */
+/* eslint-disable no-undef */
 import "../contacts.html"
 import "../scss/contacts.scss"
+
+ymaps.ready(init)
+function init() {
+	const myMap = new ymaps.Map("map", {
+		center: [55.76, 37.64],
+		zoom: 7,
+	})
+}
